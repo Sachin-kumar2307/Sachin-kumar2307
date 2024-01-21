693693dfae09a614533250e6a4f37a50d7fd1220
Sachin-kumar2307 <119109759+Sachin-kumar2307@users.noreply.github.com>
2024-01-21 12:49:17 +0530

    Add files via upload

---
+++ interview.js
@@ -0,0 +1,43 @@
+1.what is javascript?
+->javascript is client-side scripting language.it is executes in user's browser.it is processes object-oriented capabilities. it is use  for button click and screen tuch
+2.what is diffence between java vs javascript 
+java->it is object oriented programming language.File extension is .java .it can run on any virtual machine or browser. it is requires entire JDK to write a program.
+javascript->it is object based scripting language.File extension .js . it is runs on browser. it is requires only a text editor.
+3.what is important features of javascript ?
+->it is lightweight,object based scripting language. it is cross-platform. it is case-sensitive.
+4.what are scopes of a variable in javaScript 
+->scop-visibility of variables .1.local 2.global variables 
+5.what is the purpose of 'This'keyword  in javascript?
+ ->Refers to the object from where it was called 
+ 6.operator '==' vs '==='
+ ->'=='=>makes type correction 
+ '==='=>supportsstrict equality
+ 7.what is NULL in javaScript?
+ ->implies no value or no object 
+ 8.undeclared vs undefined variables 
+ ->undeclared->Not initilized or declared . throws runtime error.
+ undefined->declared but not assigned a value .returns undefined value.
+ 9.innerHTML vs innerText 
+ ->innerHTML->interprets HTML tag as HTML.sets content in HTML format.
+ innerText->interprets HTML tag as text.set the tag content as plain text.
+10.NAN in javaScript?
+->NAN- Not-a-number 
+11.break vs continue statement?
+->break->it is used to jump out of current loop without executing following statements.
+continue->skips remaining loop statements and continues with next iteration in the loop.
+12.what is use of 'typeof ' operator?
+->return string description of the operand 
+ex: typeof"TutorialRide.com" //return"string"
+    typeof 123               //return "number"
+13.what are different types of errors in javascript?
+->there are three type error in javascript
+1.syntax error->occur at interpret time
+2.Run-time error->occur during execution time ex:division of number by 0
+3.logical error ->no errors are thrown but you do nt get intended results also 
+14.what is role of strict mode in javaScript?
+->in this mode the code throws all types of errors 
+15.can you access cookies using javaScript?
+->cookies can be created,read and erasedby javascript.it is accesible through document.cookies.
+16.diffence between null and undefined ?
+->'null'->variable defined but not assigned a value.
+'undefined'->variable declared but not defined 
